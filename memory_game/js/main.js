eb59b52f9736a72cd27b52c19483bfea461704ec
Darnell <darnellatsimon@gmail.com>
2019-01-07 10:14:37 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,26 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
+var cards = ["queen", "queen", "king", "king"]
 
-var cardOne = "king";
+var cardsInPlay = []
 
-var cardOne = "king";
+var cardOne = cards[0]
 
-var cardOne = "queen";
+cardsInPlay.push(cardOne)
+// console.log("User flipped " + cardOne)
 
-console.log("User flipped " + cardOne);
+var cardTwo = cards[2]
+
+cardsInPlay.push(cardTwo)
+
+
+
+// console.log("User flipped " + cardTwo);
+ 
+
+if (cardsInPlay[0]===cardsInPlay[1]){
+ alert("You found a match!");
+ }
+ else {
+ alert("Sorry, try again.");
+ }
