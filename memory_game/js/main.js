da3336ff3fc70e56a1f8f9c90b5337e8cd7560d6
Darnell <darnellatsimon@gmail.com>
2019-01-08 14:12:39 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,104 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"]
+//  Unit 1 - said to delete in following unit so i commentted them out
+// var cardOne = "queen"
+// var cardTwo = "king"
+// var cardThree = "queen"
+// var cardFour = "king"
 
-var cardsInPlay = []
+// console.log("User flipped " + cardOne)
+// console.log("User flipped " + cardFour)
 
-var cardOne = cards[0]
+// end of Unit 1
 
-cardsInPlay.push(cardOne)
+// Unit 2  
+
+// var cards = ["queen", "queen", "king", "king"]
+// var cardsInPlay = [];
+
+// var cardOne = cards[0]
+// cardsInPlay.push(cardOne)
+
+
 // console.log("User flipped " + cardOne)
 
-var cardTwo = cards[2]
 
-cardsInPlay.push(cardTwo)
+// var cardTwo = cards[2]
+// cardsInPlay.push(cardTwo)
+
+// console.log("User flipped " + cardTwo)
 
 
+// if (cardsInPlay.length === 2){
+// 	if (cardsInPlay[0] === cardsInPlay[1]){
+// 		alert("You found a match!");
+// 	}
+// 		else {
+// 		alert("Sorry, try again.")
+// 		}
+// }
 
-// console.log("User flipped " + cardTwo);
- 
+ // end of Unit 2
 
-if (cardsInPlay[0]===cardsInPlay[1]){
- alert("You found a match!");
- }
- else {
- alert("Sorry, try again.");
- }
+// Unit 3.1
+
+// var cards = ["queen", "queen", "king", "king"]
+// var cardsInPlay = [];
+
+// var flipCard = function (cardId){
+// 	// var cardOne = cards[0]
+// 	// cardsInPlay.push(cardOne)
+// 	// console.log("User flipped " + cardOne)
+
+
+// 	// var cardTwo = cards[2]
+// 	// cardsInPlay.push(cardTwo)
+// 	// console.log("User flipped " + cardTwo)
+
+// 	console.log("User flipped " + cards[cardId])
+
+// 	if (cardsInPlay.length === 2){
+// 		if (cardsInPlay[0] === cardsInPlay[1]){
+// 		alert("You found a match!");
+// 		}
+// 			else {
+// 			alert("Sorry, try again.")
+// 			}
+// 	}
+	
+// }
+
+// flipCard(0)
+
+
+// end of Unit 3.1
+
+
+// Unit 3.2
+
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = [];
+
+var checkForMatch = function (){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+	} else {
+	alert("Sorry, try again.");
+	}
+}
+
+var flipCard = function (cardId){
+
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId])
+
+	if (cardsInPlay.length === 2){
+		checkForMatch()
+	}
+	
+
+}
+
+flipCard(0)
+flipCard(2)
+
