483b37e67e5a8ee7fedd7ebe831892956121712c
Darnell <darnellatsimon@gmail.com>
2019-01-09 22:36:05 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,140 +1,35 @@
 console.log("Up and running!");
 
-//  Unit 1 - said to delete in following unit so i commentted them out
-// var cardOne = "queen"
-// var cardTwo = "king"
-// var cardThree = "queen"
-// var cardFour = "king"
-
-// console.log("User flipped " + cardOne)
-// console.log("User flipped " + cardFour)
-
-// end of Unit 1
-
-// Unit 2  
-
-// var cards = ["queen", "queen", "king", "king"]
-// var cardsInPlay = [];
-
-// var cardOne = cards[0]
-// cardsInPlay.push(cardOne)
-
-
-// console.log("User flipped " + cardOne)
-
-
-// var cardTwo = cards[2]
-// cardsInPlay.push(cardTwo)
-
-// console.log("User flipped " + cardTwo)
-
-
-// if (cardsInPlay.length === 2){
-// 	if (cardsInPlay[0] === cardsInPlay[1]){
-// 		alert("You found a match!");
-// 	}
-// 		else {
-// 		alert("Sorry, try again.")
-// 		}
-// }
-
- // end of Unit 2
-
-// Unit 3.1
-
-// var cards = ["queen", "queen", "king", "king"]
-// var cardsInPlay = [];
-
-// var flipCard = function (cardId){
-// 	// var cardOne = cards[0]
-// 	// cardsInPlay.push(cardOne)
-// 	// console.log("User flipped " + cardOne)
-
-
-// 	// var cardTwo = cards[2]
-// 	// cardsInPlay.push(cardTwo)
-// 	// console.log("User flipped " + cardTwo)
-
-// 	console.log("User flipped " + cards[cardId])
-
-// 	if (cardsInPlay.length === 2){
-// 		if (cardsInPlay[0] === cardsInPlay[1]){
-// 		alert("You found a match!");
-// 		}
-// 			else {
-// 			alert("Sorry, try again.")
-// 			}
-// 	}
-	
-// }
-
-// flipCard(0)
-
-
-// end of Unit 3.1
-
-
-// Unit 3.2
-
-// var cards = ["queen", "queen", "king", "king"]
-// var cardsInPlay = [];
-
-// var checkForMatch = function (){
-// 	if (cardsInPlay[0] === cardsInPlay[1]) {
-// 	alert("You found a match!");
-// 	} else {
-// 	alert("Sorry, try again.");
-// 	}
-// }
-
-// var flipCard = function (cardId){
-
-// 	console.log("User flipped " + cards[cardId])
-// 	cardsInPlay.push(cards[cardId])
-
-// 	if (cardsInPlay.length === 2){
-// 		checkForMatch()
-// 	}
-	
-
-// }
-
-// flipCard(0)
-// flipCard(2)
-
-// End of 3.2
 
 var cards = [
 
 {
 	rank: "queen",
 	suit: "hearts",
-	cardId: "images/queen-of-hearts.png"
+	cardImage: "images/queen-of-hearts.png"
 
 },
 
 {
 	rank: "queen",
 	suit: "diamonds",
-	cardId: "images/queen-of-diamonds.png"
+	cardImage: "images/queen-of-diamonds.png"
 
 },
 
 {
 	rank: "king",
 	suit: "hearts",	
-	cardId: "images/king-of-hearts.png"
+	cardImage: "images/king-of-hearts.png"
 
 },
 
 {
 	rank: "king",	
 	suit: "diamonds",		
-	cardId: "images/king-of-diamonds.png"
+	cardImage: "images/king-of-diamonds.png"
 
 }
-
-
 ];
 
 var cardsInPlay = [];
@@ -145,29 +40,30 @@ var checkForMatch = function (){
 	} else {
 	alert("Sorry, try again.");
 	}
-}
+};
 
-var flipCard = function (cardId){
+var flipCard = function (){
 
-	console.log("User flipped " + cards[cardId].rank);
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
-
-	console.log(cards[cardId].cardId)
-	console.log(cards[cardId].suit)
+	this.setAttribute('src', cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2){
 		checkForMatch()
 	}
-	
+};
 
-}
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+    	var cardElement = document.createElement('img');
+    	cardElement.setAttribute('src', 'images/back.png');
+    	cardElement.setAttribute('data-id', i);
+    	cardElement.addEventListener('click', flipCard);
+    	document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+createBoard()
 
-flipCard(0)
-flipCard(2)
-
-	
-	// console.log(cards[cardId].cardImage)
-	// console.log(cards[cardId].suit)
 
 
 
